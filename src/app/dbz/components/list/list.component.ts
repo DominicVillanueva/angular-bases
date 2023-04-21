adc1854a2c4fa545c226568a06bc219ee8c46b69
DominicVillanueva <partner.dominic@hotmail.com>
2023-04-20 19:10:13 -0500

    add component dbz

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css'],
+})
+export class ListComponent {
+  @Input() public characterList: Character[] = [];
+  @Output() public onDelete: EventEmitter<string> = new EventEmitter<string>();
+
+  onDeleteCharacter(id: string = ''): void {
+    if(!id) return;
+    this.onDelete.emit(id);
+  }
+}
